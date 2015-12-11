f1f3d5cc55a53f5cad66ab8cbd042695a0581995
Rachel Morgan <rachyllmorgan@gmail.com>
2015-12-11 15:33:27 -0600

    added scrolling, iphone mockups, overall styling and project descriptions

--- javascript/app.js
+++ javascript/app.js
@@ -1,17 +1,6 @@
 $(document).ready(function() {
-
-    // $('#carousel-open-mic').carousel({
-    //     interval: 5000,
-    //     keyboard: true,
-    //     pause: "hover",
-    //     cycle: true
-    // });
-
-    // $('#carousel-beerTynder').carousel({
-    //     interval: 5000,
-    //     keyboard: true,
-    //     pause: "hover",
-    //     cycle: true
-    // });
+  // window.onload = function (){
+  //   var success = new PDFObject({ url: "sample.pdf" }).embed();
+  // };
 
 });
